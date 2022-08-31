6d317423b0d76997f7c6e4465c0ad478ae74c4e1
Olivia McKee <olivia.mckee97@gmail.com>
2022-08-31 13:49:51 -0500

    render cards and form

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,12 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/js/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            title: 'Webpack Plugin',
+        })
+    ],
     module: {
         rules: [
             {
@@ -30,12 +35,7 @@ module.exports = {
                     }
                 }
             }
+
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            title: 'Webpack Plugin',
-        })
-    ]
+    }
 };
