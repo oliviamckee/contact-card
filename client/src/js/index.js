2e9a94a8d4d022798ce8e0c19f7709b92e9bbc18
Olivia McKee <olivia.mckee97@gmail.com>
2022-08-31 14:05:25 -0500

    delete functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,6 @@
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initDb, getDb, postDb } from './database';
+import { initDb, getDb, postDb, deleteDb } from './database';
 import { fetchCards } from './cards';
 import { toggleForm, clearForm } from './form';
 
@@ -55,3 +55,12 @@ form.addEventListener('submit', event => {
     // Reload the DOM
     fetchCards();
 });
+
+window.deleteCard = (e) => {
+    // Grabs the id from the button element attached to the contact card.
+    let id = parseInt(e.id);
+    // Delete the card
+    deleteDb(id);
+    // Reload the DOM
+    fetchCards();
+};
