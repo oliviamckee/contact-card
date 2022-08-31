1c61dd60a6308054e2eba145634776c9a1a91caa
Olivia McKee <olivia.mckee97@gmail.com>
2022-08-31 16:01:44 -0500

    service worker

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -90,3 +90,10 @@ window.editCard = (e) => {
     // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
     submitBtnToUpdate = true;
 };
+
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./service-worker.js');
+    })
+};
